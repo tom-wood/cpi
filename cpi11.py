abc9e0d010576038ac2e36eaf3c8513c6a0aee82
Tom Wood <thomas.wood@stfc.ac.uk>
2017-02-03 22:03:53 +0000

    Get log filenames supported

--- cpi11.py
+++ cpi11.py
@@ -1,5 +1,5 @@
 #Version 0.1.1-beta
-#03/01/17: added get_run_numbers() method to Dataset class
+#03/02/17: added log_fname attribute and get_temps() method to Dataset
 
 import numpy as np
 import matplotlib as mpl
@@ -9,6 +9,11 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes #for colour bars
 from matplotlib import animation #for animation
 import os #for finding out which files are in a directory
 import re
+from system import platform
+if platform == 'linux2':
+    %matplotlib qt5
+else:
+    %matplotlib qt
 
 mpl.rcParams['mathtext.default'] = 'regular'
 mpl.rcParams['font.size'] = 16
@@ -78,13 +83,14 @@ class PlotDefaults():
 
 class Dataset:
     def __init__(self, filepath, first_file_number, last_file_number,
-                 suffix='-mac-001_reb_0002.xye'):
+                 suffix='-mac-001_reb_0002.xye', log_fname=None):
         self.filepath = filepath
         self.suffix = suffix
         self.expt_nums = range(first_file_number, last_file_number + 1)
         self.beam_offs = []
         self.Tvals = []
-
+        self._assign_log_fname(log_fname)
+        
     def get_run_numbers(self):
         """Return array of run numbers"""
         return np.array(self.expt_nums)
@@ -139,6 +145,40 @@ class Dataset:
                                     np.zeros(data[marker - 1].shape[0])}))
         self.data = data
         return
+
+    def _append_log_fname(self, log_fname):
+        if '\\' in log_fname:
+            app_lf = log_fname
+        elif '/' in log_fname:
+            app_lf = log_fname
+        else:
+            app_lf = self.filepath + log_fname
+        return app_lf
+
+    def _assign_log_fname(self, log_fname):
+        if log_fname:
+            if type(log_fname) == type(''):
+                log_fname = [log_fname]
+            self.log_fname = [self._append_log_fname(lf) for lf in 
+                              log_fname]
+        else:
+            self.log_fname = None
+
+    def get_temps(self, log_fname):
+        """Fetch temperature vs run number for Datset log file(s)"""
+        if log_fname:
+            _assign_log_fname(log_fname)
+        if not self.log_fname:
+            fnames = os.listdir(self.filepath)
+            self.log_fname = [self.filepath + fn for fn in fnames if 
+                              fn[-3:] == 'log']
+        log_data = []
+        for lf in self.log_fname:
+            log_data.append(pd.read_csv(lf, header=None, 
+                                        delim_whitespace=True, 
+                                        names=['rn', 'T']), usecols=[0, 1])
+        #now need to look through log_data and compare with run numbers
+
 
    #data_xy() only works if all files are the same length
     #might need future-proofing at some point.
