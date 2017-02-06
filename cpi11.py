22973b7e1475c1f9045357b62ed9510aa96a0a7a
Tom Wood <thomas.wood@stfc.ac.uk>
2017-02-06 15:20:02 +0000

    Put ipython magic into python module

--- cpi11.py
+++ cpi11.py
@@ -9,11 +9,13 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes #for colour bars
 from matplotlib import animation #for animation
 import os #for finding out which files are in a directory
 import re
-from system import platform
+from sys import platform
+from IPython import get_ipython
+ipy = get_ipython()
 if platform == 'linux2':
-    %matplotlib qt5
+    ipy.magic("matplotlib qt5")
 else:
-    %matplotlib qt
+    ipy.magic("matplotlib qt")
 
 mpl.rcParams['mathtext.default'] = 'regular'
 mpl.rcParams['font.size'] = 16
